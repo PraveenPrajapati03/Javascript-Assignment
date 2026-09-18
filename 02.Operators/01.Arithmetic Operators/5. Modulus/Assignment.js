1f44e1faf07ed7a490833d432289ed912d17c3c2
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-09-18 11:51:23 +0530

    assignment uploaded

--- 02.Operators/01.Arithmetic Operators/5. Modulus/Assignment.js
+++ 02.Operators/01.Arithmetic Operators/5. Modulus/Assignment.js
@@ -1,2 +1,32 @@
 // 5. Modulus %
 // 1
+let totalStudents=53;
+let groups=5;
+let leftStudents=totalStudents%groups;
+console.log(`The remaining students are ${leftStudents}`)
+
+// 2
+let totalCandies=128;
+let packOf=10;
+let leftCandies=totalCandies%packOf;
+console.log(`The remianing candies after packing is ${leftCandies}`)
+
+// 3
+let num=7;
+if (num%2===0){
+    console.log("The number is Even")
+}else{
+    console.log("The numnber is Odd")
+}
+
+// 4
+let totalToys=237;
+let packOfToys=6;
+let leftToys=totalToys%packOfToys;
+console.log(`The toys remaining after packing is ${leftToys}`)
+
+// 5
+let totalPassengers=185;
+let eachBus=40;
+let leftPassengers=totalPassengers%eachBus;
+console.log(`The people are left after filling in many buses ${leftPassengers}`)
---
+++ 02.Operators/01.Arithmetic Operators/6. Exponentiation/Assignment.js
@@ -0,0 +1,27 @@
+// 6. Exponentiation **
+// 1
+let cubeSide=6;
+let cubeVolume=cubeSide**3;
+console.log(`The volume of cube is ${cubeVolume}`)
+
+// 2
+let bacteria=1;
+let hours=4;
+let totalBacteria=bacteria*2**hours;
+console.log(`the bacteria after the 4 hours is ${totalBacteria}`)
+
+// 3
+let eachSideCell=9;
+let totalCells=eachSideCell**2;
+console.log(`The total number of cells are ${totalCells}`)
+
+// 4
+let num=5;
+let power=4;
+let result=num**power;
+console.log(`The 5 raised to the power 4 is ${result}`)
+
+// 5
+let eachSidePixels=1024;
+let totalPixels=eachSidePixels**2;
+console.log(`The total pixels in digital image is ${totalPixels}`)
